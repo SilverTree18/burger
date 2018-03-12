3f45d9ccd769a00cafdcb5a010af0ac9a2eab162
shannon hart <shannonehart18@gmail.com>
2018-03-11 22:35:16 -0500

    updated files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('The Lambshank Redemption');
+INSERT INTO burgers (burger_name) VALUES ('The Sweet Sunrise');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Numb Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Moist Maker', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Screaming For Vengeance Burg', true);
+INSERT INTO burgers (burger_name) VALUES ('Baby Got Bok Choy');
